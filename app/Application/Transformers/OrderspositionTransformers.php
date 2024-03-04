bef2b2386d2b61c07fe9b3b9fad46fca44369d47
mahmoud <sozokiamk@gmail.com>
2024-03-04 10:26:17 +0200

    api's issues

--- app/Application/Transformers/OrderspositionTransformers.php
+++ app/Application/Transformers/OrderspositionTransformers.php
@@ -12,12 +12,12 @@ class OrderspositionTransformers extends AbstractTransformer
     {
         return [
             "id" => $modelOrCollection->id,
-            "title" => ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses['title_lang']):null,
-            "image" => ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses['image']):null,
-            "original_price" =>  ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses['priceBase']):null,
-            "rating" =>  ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses['CourseRating']):null,
-            "courseCountRating" =>  ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses['CourseCountRating']):null,
-            "slug" =>  ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses['slug']):null,
+            "title" => ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses ? $modelOrCollection->courses['title_lang'] : ''):null,
+            "image" => ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses ? $modelOrCollection->courses['image'] : '' ):null,
+            "original_price" =>  ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses ?  $modelOrCollection->courses['priceBase'] : ''):null,
+            "rating" =>  ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses ? $modelOrCollection->courses['CourseRating'] : ''):null,
+            "courseCountRating" =>  ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses ? $modelOrCollection->courses['CourseCountRating'] : ''):null,
+            "slug" =>  ($modelOrCollection->type == Ordersposition::TYPE_Course)?($modelOrCollection->courses ? $modelOrCollection->courses['slug'] : ''):null,
 			"amount" => $modelOrCollection->amount,
 			"notes" => $modelOrCollection->notes,
 			"certificate_id" => $modelOrCollection->certificate_id,
