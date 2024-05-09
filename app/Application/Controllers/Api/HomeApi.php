a72b694b5539f7338fbe18bab9ed9ba51aaf4b21
mahmoud <sozokiamk@gmail.com>
2024-05-09 15:04:43 +0300

    certificate issue

--- app/Application/Controllers/Api/HomeApi.php
+++ app/Application/Controllers/Api/HomeApi.php
@@ -49,10 +49,10 @@ class HomeApi extends Controller
 
     public function countersHome(){
         return response(apiReturn([
-            'Students' => 1000,
-            'Courses' => 500,
-            'Hours' => 20000,
-            'Comments' => 1200,
+            'Students' => "+200,000",
+            'Courses' => "+400",
+            'Hours' => "+2000",
+            'Comments' => "180",
         ]), 200);
     }
     public function topSearches(){
