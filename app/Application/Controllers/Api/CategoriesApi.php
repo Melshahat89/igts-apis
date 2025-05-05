47b84b07016f5dfc86aecc02634c0ebec36121e8
Mahmoud <sozokiamk@gmail.com>
2025-05-05 13:03:35 +0300

    Edits

--- app/Application/Controllers/Api/CategoriesApi.php
+++ app/Application/Controllers/Api/CategoriesApi.php
@@ -26,7 +26,7 @@ class CategoriesApi extends Controller
     public function index()
     {
 //        $limit = request()->has('limit') &&  (int) request()->get('limit') != 0 && (int) request()->get('limit') < 30 ? request()->get('limit') : env('PAGINATE');
-        $data = $this->model->where('show_home',1)->orderBy('id' , 'asc')->get();
+        $data = $this->model->where('show_menu',1)->orderBy('id' , 'asc')->get();
         if ($data) {
             return response(apiReturn(CategoriesTransformers::transform($data) ), 200);
         }
